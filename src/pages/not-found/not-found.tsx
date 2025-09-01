9caf60eef6622359ff335a6fb7bbb5ce1133cbc9
WilmerAndresMoreno <Wilmer.Andres.Moreno@outlook.com>
2025-09-01 05:51:17 -0500

    changes notFoundPage

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,7 +1,15 @@
 export default function NotFound() {
 	return (
 		<>
-			<h1>404 ¡Oops, página no encontrada!</h1>
+			<div className='min-h-[60vh] flex items-center justify-center p-6'>
+				<img
+					src='/404.svg'
+					alt='Página no encontrada (error 404)'
+					className='max-w-[580px] w-full h-auto'
+					loading='eager'
+					decoding='async'
+				/>
+			</div>
 		</>
 	)
 }
